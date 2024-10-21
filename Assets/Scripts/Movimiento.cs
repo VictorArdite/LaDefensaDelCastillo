cfd14be257832fc43dede3b22ffecebecb468ac0
VictorArdite <victor_ardite@ceroca.cat>
2024-10-21 20:21:30 +0200

    Movimiento del personaje y flechas

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Movimiento : MonoBehaviour
@@ -8,19 +9,50 @@ public class Movimiento : MonoBehaviour
 
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
+    private float _vel;
+    private Vector2 minPantalla, maxPantalla;
+    [SerializeField] private GameObject prefabFlecha;
     void Start()
     {
-        Rigidbody2D = GetComponent<Rigidbody2D>();
+        _vel = 13f;
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        minPantalla.x = minPantalla.x + 1.5f;
+        maxPantalla.x = maxPantalla.x - 1.5f;
+        minPantalla.y += 1.5f;
+        maxPantalla.y -= 1.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-         Horizontal = Input.GetAxisRaw("Horizontal");
+         MoverPers();
+         DisparaFlechal();
     }
 
-    private void FixedUpdate()
+    private void MoverPers()
     {
-        Rigidbody2D.velocity = new Vector2(Horizontal,Rigidbody2D.velocity.y);
+
+        float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
+        float direccioIndicadaY = Input.GetAxisRaw("Vertical");
+
+        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY).normalized;
+
+        Vector2 novaPos = transform.position;
+        novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
+
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
+
+        transform.position = novaPos;
+    }
+    private void DisparaFlechal()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject projectil = Instantiate(prefabFlecha);
+            projectil.transform.position = transform.position;
+        }
     }
 }
